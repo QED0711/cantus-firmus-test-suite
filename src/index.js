a5dadcc2773ecaf644e9ae09e284276f43c16f5a
QED0711 <qdizon@gmail.com>
2021-02-03 06:39:42 -0500

    updated default index and app files

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import { MainProvider } from './state/main/mainProvider';
 
 ReactDOM.render(
-  <MainProvider>
-    <App />
-  </MainProvider>,
+  <App />,
   document.getElementById('root')
 );
 
