f8cca74e46c962ba61d469d14eec1d1e498658c2
QED0711 <qdizon@gmail.com>
2021-02-05 06:29:38 -0500

    added tests for dynamic, asyc, and nested setters

--- src/components/SettersTest.js
+++ src/components/SettersTest.js
@@ -2,20 +2,60 @@ import { useContext } from 'react'
 import { MainContext, MainProvider } from '../state/main/mainProvider'
 
 const SettersChild = () => {
-    
-    const {state, setters} = useContext(MainContext)
-    
-    const handleClick = () => {
+
+    const { state, setters, getters } = useContext(MainContext)
+
+    const handleSingleUpdate = () => {
         setters.setNum1(1)
     }
-    
+
+    const handleCustomSetter = () => {
+        setters.customSetter()
+    }
+
+    const handleAsyncSetter = () => {
+        setters.asyncSetter()
+    }
+
+    const handleDynamicAsyncSetter = async () => {
+        // update state 5 times asynchronously
+        // have to use getters so we're always getting the latest state value
+        for (let i = 0; i < 5; i++) {
+            await setters.setNum1(getters.getNum1() + 1)
+        }
+    }
+
+    const handleDynamicNestedSetter = () => {
+        setters.setNested_str2("Updated a nested value!")
+    }
+
+
     return (
         <div>
-            <button onClick={handleClick}>Single Update</button>
-            <br/>
-            <h1>{state.num1}</h1>
+            <button onClick={handleSingleUpdate}>Single Update</button>
+            <br />
+
+            <button id="customSetterButton" onClick={handleCustomSetter}>Custom Setters</button>
+            <br />
+
+            <button id="asyncSetterButton" onClick={handleAsyncSetter}>Custom Setters</button>
+            <br />
+
+            <button id="asyncDynamicSetterButton" onClick={handleDynamicAsyncSetter}>Custom Setters</button>
+            <br />
+            
+            <button id="dynamicNestedSetter" onClick={handleDynamicNestedSetter}>Custom Setters</button>
+            <br />
+
+
+            <h1 id="num1">{state.num1}</h1>
+            <h1 id="str1">{state.str1}</h1>
+            <h1 id="nestedStr2">{state.nested.str2}</h1>
+            
         </div>
     )
+
+
 
 }
 
--- src/test/Setters.test.js
+++ src/test/Setters.test.js
@@ -1,26 +1,56 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import SettersTest from '../components/SettersTest';
 
 test("Dynamic setters correctly set their linked state property", () => {
-    render(<SettersTest />)
+    const dom = render(<SettersTest />)
 
     fireEvent.click(screen.getByText("Single Update"))
 
-    const updatedNum1 = screen.getByText(/1/)
+    const num1 = parseInt(dom.container.querySelector("#num1").innerHTML)
 
-    expect(updatedNum1).toBeInTheDocument()
-
-    
+    expect(num1).toEqual(1)    
 })
 
 test("Custom setters can update state with `this.setState`", () => {
+    const dom = render(<SettersTest />)
 
+    fireEvent.click(dom.container.querySelector("#customSetterButton"))
+
+    const str1 = dom.container.querySelector("#str1").innerHTML
+    const num1 = parseInt(dom.container.querySelector("#num1").innerHTML)
+
+    expect(str1).toEqual("new string")
+    expect(num1).toEqual(100)
 })
 
-test("`this.setState` is asynchronous", () => {
+test("`this.setState` is asynchronous", async () => {
+    const dom = render(<SettersTest />)
 
+    fireEvent.click(dom.container.querySelector("#asyncSetterButton"))
+    
+    await waitFor(() => {
+        const num1 = parseInt(dom.container.querySelector("#num1").innerHTML)
+        expect(num1).toEqual(3)
+    })
+})
+
+test("Dynamic setters are asynchronous", async () => {
+    const dom = render(<SettersTest />)
+
+    fireEvent.click(dom.container.querySelector("#asyncDynamicSetterButton"))
+
+    await waitFor(() => {
+        const num1 = parseInt(dom.container.querySelector("#num1").innerHTML)
+        expect(num1).toEqual(5)
+    })
 })
 
 test("Dynamic nested setters set their linked state property", () => {
-    
+    const dom = render(<SettersTest />)
+
+    fireEvent.click(dom.container.querySelector("#dynamicNestedSetter"))
+
+    const nestedStr2 = dom.container.querySelector("#nestedStr2").innerHTML
+
+    expect(nestedStr2).toEqual("Updated a nested value!")
 })
