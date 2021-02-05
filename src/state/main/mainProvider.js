3dd44f56e8308a98753aea38dad7c82d092e373f
QED0711 <qdizon@gmail.com>
2021-02-05 06:27:41 -0500

    updated to include dynamic setters

--- src/state/main/mainProvider.js
+++ src/state/main/mainProvider.js
@@ -7,12 +7,14 @@ import methods from './methods'
 import reducers from './reducers'
 import constants from './constants'
 
-const main = new CantusFirmus(state)
+const main = new CantusFirmus(state, {nestedSetters: true})
 
 main.addCustomSetters(setters)
 main.addMethods(methods)
 main.addReducers(reducers)
 main.addConstants(constants)
+
+main.connectToLocalStorage({name: "main"})
 
 export const MainContext = main.context;
 export const MainProvider = main.createProvider();
