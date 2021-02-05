aa7614ddfcb1f59ea8b91c6639906ff7d5ca2d7e
QED0711 <qdizon@gmail.com>
2021-02-05 06:28:56 -0500

    added nested state values

--- src/state/main/state.js
+++ src/state/main/state.js
@@ -2,8 +2,12 @@
 const state = {
 
     num1: 0,
-    str1: "Hello, world"
+    str1: "Hello, world",
 
+    nested: {
+        num2: -1,
+        str2: "I'm nested one level",
+    }
 }
 
 export default state;
