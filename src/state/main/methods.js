c6c4c26ea39ffc827febe819b286875654b6b9bb
QED0711 <qdizon@gmail.com>
2021-02-08 05:12:15 -0500

    added methods and namespaced methods

--- src/state/main/methods.js
+++ src/state/main/methods.js
@@ -1,8 +1,34 @@
 
 const methods = {
 
-    // your methods here...
+    get(key){
+        return this.state[key]
+    },
+
+    async incrementCountBy2(){
+        await this.setters.setNum1(this.state.num1 + 1)
+        await this.setters.setNum1(this.state.num1 + 1)
+    },
+
+    thisKeyword(){
+        return Object.keys(this)
+    }
 
 }
 
 export default methods;
+
+
+export const namespacedMethods = {
+    someNamespacedMethods: {
+        retrieveValue(key){
+            return this.otherNamespacedMethods.retrieveValue(key)
+        }
+    },
+
+    otherNamespacedMethods: {
+        retrieveValue(key){
+            return this.state[key]
+        }
+    }
+}
