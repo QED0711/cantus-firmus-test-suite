62515146c05a22d74e6a7bc7b3fd808a335c4c19
QED0711 <qdizon@gmail.com>
2021-02-08 05:11:13 -0500

    separated out methods and namespaced methods

--- src/state/main/mainProvider.js
+++ src/state/main/mainProvider.js
@@ -3,16 +3,20 @@ import CantusFirmus from 'cantus-firmus';
 
 import state from './state'
 import setters from './setters'
-import methods from './methods'
+import methods, {namespacedMethods} from './methods'
 import reducers from './reducers'
 import constants from './constants'
 
 const main = new CantusFirmus(state, {nestedSetters: true})
 
 main.addCustomSetters(setters)
+
 main.addMethods(methods)
+main.addNamespacedMethods(namespacedMethods)
+
 main.addReducers(reducers)
 main.addConstants(constants)
+
 
 main.connectToLocalStorage({name: "main", subscriberWindows: ["child", "grandchild"]})
 
