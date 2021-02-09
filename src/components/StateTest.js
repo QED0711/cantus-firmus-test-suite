87afaaa66962b6227864215a0605cee9ebe3b4c1
QED0711 <qdizon@gmail.com>
2021-02-09 05:29:45 -0500

    added dynamic getter tests

--- src/components/StateTest.js
+++ src/components/StateTest.js
@@ -4,11 +4,19 @@ import { MainContext, MainProvider } from '../state/main/mainProvider';
 
 
 const StateChild = () => {
-    const { state } = useContext(MainContext)
+    const { state, getters } = useContext(MainContext)
     return (
         <>
-            <h1>{state.num1}</h1>
-            <h1>{state.str1}</h1>
+            <h1 id="state-num1">{state.num1}</h1>
+            <h1 id="state-str1">{state.str1}</h1>
+            
+            <h1 id="getters-num1">{getters.getNum1()}</h1>
+            <h1 id="getters-str1">{getters.getStr1()}</h1>
+            
+            <h1 id="nested-num2">{getters.getNested_num2()}</h1>
+            <h1 id="nested-str2">{getters.getNested_str2()}</h1>
+
+
         </>
     )
 }
--- src/test/State.test.js
+++ src/test/State.test.js
@@ -2,11 +2,32 @@ import { render, screen } from '@testing-library/react';
 import StateTest from '../components/StateTest';
 
 test("Retrieves correct values from state", () => {
-    render(<StateTest />);
+    const dom = render(<StateTest/>)
 
-    const num1 = screen.getByText(/0/);
-    const str1 = screen.getByText(/hello, world/i)
+    const stateNum1 = dom.container.querySelector("#state-num1").innerHTML 
+    const stateStr1 = dom.container.querySelector("#state-str1").innerHTML
+    
+    expect(stateNum1).toEqual("0")
+    expect(stateStr1).toEqual("Hello, world")
+})
 
-    expect(num1).toBeInTheDocument()
-    expect(str1).toBeInTheDocument()
+test("Dynamic getters are correctly associated with state values", () => {
+    const dom = render(<StateTest/>)
+
+    const gettersNum1 = dom.container.querySelector("#getters-num1").innerHTML 
+    const gettersStr1 = dom.container.querySelector("#getters-str1").innerHTML
+    
+    expect(gettersNum1).toEqual("0")
+    expect(gettersStr1).toEqual("Hello, world")
+})
+
+test("Nested dynamic getters are correctly associated with state values", () => {
+    const dom = render(<StateTest/>)
+
+    const nestedNum2 = dom.container.querySelector("#nested-num2").innerHTML
+    const nestedStr2 = dom.container.querySelector("#nested-str2").innerHTML
+
+    expect(nestedNum2).toEqual("-1")
+    expect(nestedStr2).toEqual("I'm nested one level")
+
 })
