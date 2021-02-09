bd08b251de44be78ee213d4c0c1213bc5a20c74a
QED0711 <qdizon@gmail.com>
2021-02-09 06:18:12 -0500

    added state properties to be ignored to test ignoreGetters and ignoreSetters

--- src/state/main/state.js
+++ src/state/main/state.js
@@ -3,11 +3,15 @@ const state = {
 
     num1: 0,
     str1: "Hello, world",
-
+    
+    ignoreMe: "No setter for me",
+    
     nested: {
         num2: -1,
         str2: "I'm nested one level",
-    }
+        ignoreMeToo: "No setter for me either"
+    },
+
 }
 
 export default state;
