56972a6aa1f883186b4f03a18b178106dd20a36b
QED0711 <qdizon@gmail.com>
2021-02-10 06:08:57 -0500

    added custom reducers

--- src/state/main/reducers.js
+++ src/state/main/reducers.js
@@ -1,7 +1,33 @@
 
 const reducers = {
 
-    // your reducers here...
+    numReducer(state = {}, action) {
+        switch (action.type) {
+            case "UPDATE_NUM_1":
+                return { ...state, num1: action.payload };
+            case "UPDATE_NESTED_NUM_2":
+                const newState = JSON.parse(JSON.stringify(state))
+                newState.nested.num2 = action.payload
+                return newState;
+            default:
+                return state;
+        }
+    },
+
+    strReducer(state = {}, action){
+        switch (action.type) {
+            case "UPDATE_STR_1":
+                return { ...state, str1: action.payload };
+            case "UPDATE_NESTED_STR_2":
+                const newState = JSON.parse(JSON.stringify(state))
+                newState.nested.str2 = action.payload
+                return newState;
+            default:
+                return state;
+        }
+    }
+
+
 
 }
 
